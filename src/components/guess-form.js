1d7856473ed362394b41929dd15324f370ac4e66
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-01-25 09:29:56 -0800

    Update guess-form to clear input field on submit

--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -3,16 +3,19 @@ import React from 'react';
 import './guess-form.css';
 
 export default function GuessForm(props) {
-    return (
+  return (
         <form onSubmit={(e)=> {
-            e.preventDefault();
-            console.log(document.querySelector('[name=userGuess]').value);
-            props.onSubmit(document.querySelector('[name=userGuess]').value)}}>
+          e.preventDefault();
+          console.log(document.querySelector('[name=userGuess]').value);
+          props.onSubmit(document.querySelector('[name=userGuess]').value);
+          document.querySelector('[name=userGuess]').value='';
+        }
+            }>
             <input type="text" name="userGuess" id="userGuess"
                 className="text" maxLength="3" autoComplete="off"
                 placeholder="Enter your Guess" required />
             <input  type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
         </form>
     );
-};
+}
 
